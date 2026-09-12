14afdeb3ea9c1fb1c21bb907ecbbcaea59de279e
xxblebleblexx <cicaknempel2@gmail.com>
2026-09-12 13:34:17 +0700

    block: import ssg ioscheduler and set as default
    Signed-off-by: xxblebleblexx <cicaknempel2@gmail.com>

--- block/elevator.c
+++ block/elevator.c
@@ -991,12 +991,8 @@ int elevator_init_mq(struct request_queue *q)
 
 	if (unlikely(q->elevator))
 		goto out;
-	if (IS_ENABLED(CONFIG_IOSCHED_BFQ)) {
-		e = elevator_get(q, "bfq", false);
-		if (!e)
-			goto out;
-	} else {
-		e = elevator_get(q, "mq-deadline", false);
+
+		e = elevator_get(q, "ssg", false);
 		if (!e)
 			goto out;
 	}
